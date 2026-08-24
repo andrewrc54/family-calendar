f2a819a6ae7fd5a8cd70e45ca112edc39fbc119d
Andrew Ramos <andrewramos@MacBook-Pro-de-Andrew-2.local>
2026-08-24 17:12:40 -0500

    Configurar Client ID de Google OAuth

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Consíguelo en https://console.cloud.google.com/apis/credentials
 // -> Crear credenciales -> ID de cliente de OAuth -> Aplicación web
 // -> Orígenes de JavaScript autorizados: la URL exacta donde quede publicada esta app.
-const GOOGLE_CLIENT_ID = "REEMPLAZA_CON_TU_CLIENT_ID.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "567946621551-mmmjk0a0ugfokg6vo0pjf4r6ldb1hp57.apps.googleusercontent.com";
 
 const GOOGLE_CALENDAR_SCOPES = "https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.calendarlist.readonly";
